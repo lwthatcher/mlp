36abc21d843c04720dc112e5fd3ba689a531ed77
Lawrence Thatcher <lwthatcher@msn.com>
2017-03-21 13:21:34 -0600

    added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Setup for GFMM"""
+
+from distutils.core import setup
+
+setup(
+    name='mlp',
+    version='0.0.1',
+    description='Simple Multi-Layer Perceptron (MLP) neural network implementation',
+    author='Lawrence Thatcher',
+    author_email='lwthatcher@msn.com',
+    url='https://github.com/lwthatcher/mlp',
+    packages=['mlp']
+)
