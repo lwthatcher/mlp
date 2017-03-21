9bdbc6912689befe4a4bd0e9fe304ac55c34bba8
Lawrence Thatcher <lwthatcher@msn.com>
2017-03-21 13:18:49 -0600

    added load-data-file( ) function

--- mlp/mlp.py
+++ mlp/mlp.py
@@ -1,11 +1,38 @@
 import numpy as np
+import argparse
+import csv
 
 
 class NeuralNet:
 
-    def __init__(self):
+    def __init__(self, num_input_nodes, num_output_nodes, num_hidden_nodes=20, learning_rate=0.9):
+        pass
+
+    def fit(self, X, Y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def _forward_prop(self, x):
+        pass
+
+    def _back_prop(self, y):
         pass
 
 
+def load_data_file(file):
+    features = []
+    labels = []
+    with open(file, newline='') as csv_file:
+        data_reader = csv.reader(csv_file)
+        for row in data_reader:
+            labels.append(row[-1])
+            features.append(row[:-1])
+    return np.array(features), np.array(labels)
+
 if __name__ == '__main__':
-    print("MLP Neural Network coming soon!")
+    parser = argparse.ArgumentParser(description='Run a multilayer perceptron via vector operations')
+    parser.add_argument('data', help='file containing data')
+    parser.add_argument('--num_hidden', '-h', type=int, help='number of hidden nodes to use')
+    args = parser.parse_args()
