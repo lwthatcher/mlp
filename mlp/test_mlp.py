a302d7bff91147d03ab42bc41e223513b718dea1
Lawrence Thatcher <lwthatcher@msn.com>
2017-03-21 16:04:41 -0600

    created test file

---
+++ mlp/test_mlp.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class TestNeuralNet(TestCase):
+    def test__num_and_names(self):
+        self.fail()
+
+
+class TestCLI(TestCase):
+    def test_load_data_file(self):
+        self.fail()
