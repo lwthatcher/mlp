83873e371a887d06d4e71cd6860b30dbc54aef5d
Lawrence Thatcher <lwthatcher@msn.com>
2017-03-25 16:41:17 -0600

    added Sigmoid activation function

--- mlp/activation_functions.py
+++ mlp/activation_functions.py
@@ -1,11 +1,24 @@
+from scipy.special import expit
+
 
 class ReLU:
-
     @staticmethod
     def activation(net):
         net[net < 0] = 0
+        return net
 
     @staticmethod
     def f_prime(net):
         net[net <= 0] = 0
         net[net > 0] = 1
+        return net
+
+
+class Sigmoid:
+    @staticmethod
+    def activation(net):
+        return expit(net, net)
+
+    @staticmethod
+    def f_prime(net):
+        net *= (1. - net)
--- tests/test_activation_functions.py
+++ tests/test_activation_functions.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 import numpy as np
-from mlp.activation_functions import ReLU
+from mlp.activation_functions import ReLU, Sigmoid
 
 
 class TestReLU(TestCase):
@@ -8,8 +8,32 @@ class TestReLU(TestCase):
         x = np.array([[-4, 5]])
         ReLU.activation(x)
         np.testing.assert_array_equal(x, np.array([[0, 5]]))
+        a = np.array([[-2, 5]])
+        b = np.array([[-2, 5]])
+        y = ReLU.activation(a + b)
+        np.testing.assert_array_equal(y, np.array([[0, 10]]))
+        np.testing.assert_array_equal(a, np.array([[-2, 5]]))
+        np.testing.assert_array_equal(b, np.array([[-2, 5]]))
 
     def test_f_prime(self):
         x = np.array([[-4, 5]])
         ReLU.f_prime(x)
         np.testing.assert_array_equal(x, np.array([[0, 1]]))
+
+
+class TestSigmoid(TestCase):
+    def test_activation(self):
+        x = np.array([[-.8, .62005]])
+        Sigmoid.activation(x)
+        np.testing.assert_array_almost_equal(x, np.array([[.310026, .65023]]))
+        a = np.array([[-.4, .62]])
+        b = np.array([[-.4, .00005]])
+        y = Sigmoid.activation(a + b)
+        np.testing.assert_array_almost_equal(y, np.array([[.310026, .65023]]))
+        np.testing.assert_array_equal(a, np.array([[-.4, .62]]))
+        np.testing.assert_array_equal(b, np.array([[-.4, .00005]]))
+
+    def test_f_prime(self):
+        x = np.array([[.1, .3, .5, 1]])
+        Sigmoid.f_prime(x)
+        np.testing.assert_array_almost_equal(x, np.array([[.09, .21, .25, 0]]))
